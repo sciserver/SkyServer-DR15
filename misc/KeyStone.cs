10da7304ef4cc0eaec15419058c97d2fa0b32245
mtaghiza <mtaghiza@jhu.edu>
2018-10-17 10:32:43 -0400

    switch from KeystoneAPI to LoginPortal API when authenticating user token

--- misc/KeyStone.cs
+++ misc/KeyStone.cs
@@ -5,12 +5,15 @@ using System.Web;
 using net.openstack.Core.Domain;
 using net.openstack.Providers.Rackspace;
 using System.Configuration;
-
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
 
 namespace SkyServer
 {
     public class Keystone
     {
+
+        /*
         public static UserAccess Authenticate(string token)
         {
             var identityProvider = new CloudIdentityProvider(new Uri(ConfigurationManager.AppSettings["Keystone.Uri"]));
@@ -27,6 +30,110 @@ namespace SkyServer
                 throw new Exception("You do not have the permission to access Skyserver");
             }
             return userAccess;
-        }       
+        }
+        */
+
+        public static UserAccess Authenticate(string token)
+        {
+            using (var client = new HttpClient() { BaseAddress = new Uri(ConfigurationManager.AppSettings["Keystone.Portal"]) })
+            {
+                try
+                {
+                    var request = new HttpRequestMessage(HttpMethod.Get, "keystone/v3/tokens/" + token);
+
+                    var response = client.SendAsync(request).Result.EnsureSuccessStatusCode();
+
+                    JObject json = JObject.Parse(response.Content.ReadAsStringAsync().Result);
+
+                    UserAccess userAccess = new UserAccess(json);
+
+                    return userAccess;
+                }
+                catch
+                {
+                    throw new Exception("Could not authenticate token with Authentication provider");
+                }
+            }
+        }
+
+        public static bool IsValidToken(string token)
+        {
+            if (string.IsNullOrEmpty(token)) return false;
+
+            try
+            {
+                using (var client = new HttpClient() { BaseAddress = new Uri(ConfigurationManager.AppSettings["LoginPortal.Base"]) })
+                {
+                    var request = new HttpRequestMessage(HttpMethod.Head, "keystone/v3/tokens/" + token);
+                    var response = client.SendAsync(request).Result;
+                    return response.IsSuccessStatusCode;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+
+
     }
+
+
+
+    public class UserAccess
+    {
+        public User User = new User();
+        public UserAccess(JObject json)
+        {
+            string id = (string)json["token"]["user"]["id"];
+            string name = (string)json["token"]["user"]["name"];
+
+            User.setId(id);
+            User.setName(name);
+        }
+    }
+
+
+    public class User
+    {
+        public string Id = null;
+        public string Name = null;
+
+        public User()
+        {
+
+        }
+        public User(string id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+
+        public void setId(string id)
+        {
+            this.Id = id;
+        }
+        public void setName(string name)
+        {
+            this.Name = name;
+        }
+
+        public string getId()
+        {
+            return this.Id;
+        }
+
+        public string getName()
+        {
+            return this.Name;
+        }
+
+
+
+    }
+
+
+
+
 }
